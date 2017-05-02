040a82521899d50694c8f12f0b0a21622bb86e0c
juafernc <juafernc@fiv.upv.es>
2017-05-02 21:34:29 +0200

    Logger version 1.0

---
+++ Log.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf8
+__author__ = "Juan Manuel Fernández Nácher"
+
+# Librerías del sistema
+import os, time
+
+class Log:
+
+	def __init__(self, name):
+		self.name = name
+
+	def Enter(self):
+		self.__print("ENTER")
+	
+	def Exit(self):
+		self.__print("EXIT ")
+
+	def Trace(self, message):
+		self.__print("TRACE", message)
+
+	def Debug(self, message):
+		self.__print("DEBUG", message)
+
+	def Info(self, message):
+		self.__print("INFO ", message)
+
+	def Warn(self, message):
+		self.__print("WARN ", message)
+
+	def Error(self, message):
+		self.__print("ERROR", message)
+
+	def Fatal(self, message):
+		self.__print("FATAL", message)
+
+	def __print(self, type, message = ""):
+		if not os.path.exists("logs"):
+			os.mkdir("logs")
+		
+		fichero = time.strftime("%Y-%m-%d.log")
+		fecha = time.strftime("%Y-%m-%d")
+		hora = time.strftime("%H:%M:%S")
+		
+		fich = open("logs/"+fichero, "a")
+		
+		if message == "":
+			fich.write("{0} {1} {2} {3}\n".format(fecha, hora, type, self.name))
+		else:
+			fich.write("{0} {1} {2} {3} : {4}\n".format(fecha, hora, type, self.name, message))
+		
+		fich.close()
